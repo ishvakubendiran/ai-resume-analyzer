f888bff05188a9a8cc216ceb1a569d05c2d3a243
Ishva Kubendiran <ishvakubendiran@gmail.com>
2026-06-06 22:04:41 +0530

    Add PDF upload feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import { useState } from "react"
+import * as pdfjsLib from "pdfjs-dist"
+pdfjsLib.GlobalWorkerOptions.workerSrc = new URL('pdfjs-dist/build/pdf.worker.mjs', import.meta.url).toString()
 
 function App() {
   const [resume, setResume] = useState("")
   const [result, setResult] = useState("")
   const [loading, setLoading] = useState(false)
-
+  const handlePdfUpload = async (e) => {
+  const file = e.target.files[0]
+  if(!file) return
+  const arrayBuffer = await file.arrayBuffer()
+  const pdf = await pdfjsLib.getDocument({ data: arrayBuffer }).promise
+  let text = ""
+  for (let i = 1; i <= pdf.numPages; i++) {
+    const page = await pdf.getPage(i)
+    const content = await page.getTextContent()
+    text += content.items.map((item) => item.str).join(" ") + "\n"
+    }
+    console.log(text)
+    setResume(text)
+}
   const analyzeResume = async () => {
     setLoading(true)
     const apiKey = import.meta.env.VITE_GROQ_API_KEY
@@ -49,6 +64,12 @@ Resume: ${resume}`
         <p className="text-center text-gray-400 mb-8">
           Paste your resume and get your ATS score instantly
         </p>
+        <input
+        type="file"
+        accept=".pdf"
+        onChange={handlePdfUpload}
+        className="mb-4 w-full text-white"
+        />
         <textarea
           className="w-full h-64 p-4 bg-gray-800 text-white rounded-xl border border-gray-600"
           placeholder="Paste your resume here..."
